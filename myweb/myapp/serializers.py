1c7cbb13f6abf460da634dc0603174c35c380bcb
Sanjeev-pedhapally <pedhapallysanjeevkumar@gmail.com>
2024-08-22 15:59:19 -0500

    Delete myweb directory

--- myweb/myapp/serializers.py
+++
@@ -1,8 +0,0 @@
-# myapp/serializers.py
-from rest_framework import serializers
-from .models import EPLResult
-
-class EPLResultSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EPLResult
-        fields = '__all__'
